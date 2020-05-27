c99e040ac074311d1b285d3c64175ef9df3bb7c7
jxu <7989982+jxu@users.noreply.github.com>
2020-05-27 01:35:06 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,74 @@
+var CONFIG = {
+  "maps": {
+    "map1": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1590471784,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "map1",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "world_topdown_t1",
+      "tileSize": 192,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    },
+    "map2": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1590471784,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "map2",
+      "renderView": "topdown",
+      "rotations": [
+        0
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "world_topdown_t1",
+      "tileSize": 96,
+      "world": "world",
+      "worldName": "world",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map1",
+    "map2"
+  ],
+  "tileSetGroups": {
+    "world_topdown_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
